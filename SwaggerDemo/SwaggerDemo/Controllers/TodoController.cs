339c7fba52d630e6c175e59fb502fb85aaa283eb
Daniel Mueller <daniel.mueller@sdx-ag.de>
2018-05-28 12:06:49 +0200

    initial setup

---
+++ SwaggerDemo/SwaggerDemo/Controllers/TodoController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerDemo.Models;
+
+namespace SwaggerDemo.Controllers
+{
+	[Route("api/[controller]")]
+	[Controller]
+	public class TodoController : ControllerBase
+	{
+		private readonly TodoContext _context;
+
+
+		public TodoController(TodoContext context)
+		{
+			_context = context;
+
+			if (_context.TodoItems.Count() == 0)
+			{
+				_context.TodoItems.Add(new TodoItem { Name = "Item1" });
+				_context.SaveChanges();
+			}
+		}
+
+		#region snippet_GetAll
+		[HttpGet]
+		public List<TodoItem> GetAll()
+		{
+			return _context.TodoItems.ToList();
+		}
+
+		#region snippet_GetByID
+		[HttpGet("{id}", Name = "GetTodo")]
+		public TodoItem GetById(long id)
+		{
+			var item = _context.TodoItems.Find(id);
+			if (item == null)
+			{
+				return null;
+			}
+			return item;
+		}
+		#endregion
+		#endregion
+
+		#region snippet_Create
+		[HttpPost]
+		public IActionResult Create(TodoItem item)
+		{
+			_context.TodoItems.Add(item);
+			_context.SaveChanges();
+
+			return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+		}
+		#endregion
+
+		#region snippet_Update
+		[HttpPut("{id}")]
+		public IActionResult Update(long id, TodoItem item)
+		{
+			var todo = _context.TodoItems.Find(id);
+			if (todo == null)
+			{
+				return NotFound();
+			}
+
+			todo.IsComplete = item.IsComplete;
+			todo.Name = item.Name;
+
+			_context.TodoItems.Update(todo);
+			_context.SaveChanges();
+			return NoContent();
+		}
+		#endregion
+
+		#region snippet_Delete
+		[HttpDelete("{id}")]
+		public IActionResult Delete(long id)
+		{
+			var todo = _context.TodoItems.Find(id);
+			if (todo == null)
+			{
+				return NotFound();
+			}
+
+			_context.TodoItems.Remove(todo);
+			_context.SaveChanges();
+			return NoContent();
+		}
+		#endregion
+	}
+}
---
+++ SwaggerDemo/SwaggerDemo/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SwaggerDemo.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SwaggerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+	        services.AddDbContext<TodoContext>(opt =>
+		        opt.UseInMemoryDatabase("TodoList"));
+			services.AddMvc();
+
+
+	        services.AddSwaggerGen(c =>
+	        {
+		        c.SwaggerDoc("v1", new Info { Title = "Todo API", Version = "v1" });
+	        });
+		}
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contacts API V1");
+			});
+			app.UseSwagger();
+		}
+    }
+}
