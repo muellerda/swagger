97191b764f3530eea07e1214d2310e944876d8f2
Falco Prescher <falcoprescher@falcoprescher@hotmail.com>
2018-06-04 23:39:24 +0200

    Initial routing, navbar and todo component

--- Clients/Angular/src/app/app.module.ts
+++ Clients/Angular/src/app/app.module.ts
@@ -1,14 +1,24 @@
+import 'bootstrap';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TodoModule } from './todo/todo.module';
+import { ControlsModule } from './controls/controls.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    CommonModule,
+    ControlsModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
