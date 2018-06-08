4ef09f02be192a5fdc29414b10b5df6b5e6e4021
Falco Prescher <falcoprescher@googlemail.com>
2018-06-08 12:27:27 +0200

    Implementing delete

--- Clients/Angular/src/app/todo/list/list.component.ts
+++ Clients/Angular/src/app/todo/list/list.component.ts
@@ -19,11 +19,20 @@ export class ListComponent implements OnInit {
     this.todoItems = this.todoService.apiTodoGet();
   }
 
-  public async addTodo(text: string) {
+  public async addTodo() {
     try {
       await this.todoService.apiTodoPost(null, this.currentTodoItem.name).toPromise();
       this.todoItems = this.todoService.apiTodoGet();
       this.currentTodoItem = { name: '' };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  public async deleteTodo(id: number) {
+    try {
+      await this.todoService.apiTodoByIdDelete(id).toPromise();
+      this.todoItems = this.todoService.apiTodoGet();
     } catch (err) {
       console.log(err);
     }
